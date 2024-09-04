4c8cc2e3d24a6925e89b0fe413cf5d222b0196e4
Sixtus Agbo <miracleagbosixtus@gmail.com>
2024-09-04 01:21:17 +0000

    Work with some list functions

--- lists/manipulate.py
+++ lists/manipulate.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Manipulating Python lists."""
+"""Manipulating Python lists"""
 
 letters = ["a", "b", "c", "d", "e", "f"]
 
@@ -11,5 +11,64 @@ letters = ["a", "b", "c", "d", "e", "f"]
 # print(letters.pop())
 # del letters[:3]  # O(n) time, O(1) space you can delete multiple elements by slicing
 # del letters[2]  # O(n) time, O(1) space Remove the element at index 2
-letters.remove("b")  # O(n) time, O(1) space
-print(letters)
+# letters.remove("b")  # O(n) time, O(1) space
+# print(letters)
+
+# Searching for an element in a list
+# in operator
+"""
+target = "c"
+if target in letters:  # O(n) time, O(1) space
+    print(f"{target} found in the list")
+else:
+    print(f"{target} not found in the list")
+"""
+
+
+"""
+# index method
+def find_element(lst, target):  # O(n) time, O(1) space
+    for index, element in enumerate(lst):
+        if element == target:
+            return index
+    return -1
+
+
+# print(find_element(letters, "z"))
+print(find_element(letters, "e"))
+"""
+
+# List operations/functions
+# + operator
+"""
+a = [1, 2, 3]
+b = [4, 5, 6, 7]
+c = a + b
+print(c)
+"""
+
+# * operator
+"""
+a = [0, 1]
+a = a * 4
+print(a)
+"""
+
+# len function
+"""
+a = [1, 2, 3, 4, 5]
+print(len(a))
+"""
+
+# min and max functions
+"""
+a = [1, 2, 3, 4, 5]
+print(min(a))
+print(max(a))
+"""
+
+# sum function
+a = [2, 4, 10]
+print(sum(a))
+# We can combine to get the average
+print(sum(a) / len(a))
